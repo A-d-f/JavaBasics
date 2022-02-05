1f84ecd515bb50e37645eb66bf788915946321ef
A-d-f <annamaria.nappila@gmail.com>
2022-02-05 18:07:09 +0200

    fixing problems

--- Sääpalvelu/src/sääpalvelutesti/Saapalvelu.java
+++ Sääpalvelu/src/sääpalvelutesti/Saapalvelu.java
@@ -1,42 +1,42 @@
 package sääpalvelutesti;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.OptionalDouble;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Saapalvelu {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int freeze = 100;
+		int freeze = 4;
 		ArrayList<Double> saapalvelu = new ArrayList<Double>();
 		double annetut;
-		int paiva=1;
+		int day=1;
 		
 		
 		do
 			
 			{
-				System.out.println("Anna "+ paiva + " päivän lämpötila: ");
+				System.out.println("Anna "+ day + " päivän lämpötila: ");
 					annetut = Double.parseDouble(in.nextLine());
 						saapalvelu.add(annetut);
-						paiva++;
+						day++;
 						
 		 
-			} while (annetut!=freeze);
+			} while (day!=freeze);
 		
-		int index = saapalvelu.size() - 1;
-		saapalvelu.remove(index);
+		//int index = saapalvelu.size()-1;
+		//saapalvelu.remove(index);
 		for(int i=0; i<saapalvelu.size();i++)
 			System.out.println(saapalvelu.get(i));
 		
 		int index_max = saapalvelu.indexOf(Collections.max(saapalvelu));
 		index_max++;
 		System.out.println("lämpimintä oli "+ index_max + " päivänä " + getMax(saapalvelu) + " astetta");
-		
-		System.out.println("Lämpötilojen keskiarvo: "+ calculateAverage(saapalvelu));
+		DecimalFormat df = new DecimalFormat("0.1");
+		System.out.println("Lämpötilojen keskiarvo: "+ df.format(calculateAverage(saapalvelu)));
 		
 		
 		
