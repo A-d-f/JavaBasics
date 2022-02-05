f9267d06cd0b0bd00134e833155b3062c4efd53e
A-d-f <annamaria.nappila@gmail.com>
2022-02-05 17:52:50 +0200

    check

--- Lucky7/src/Lucky7.java
+++ Lucky7/src/Lucky7.java
@@ -3,81 +3,56 @@ import java.util.Scanner;
 
 public class Lucky7 {
 	public static void main(String[] args) {
-		
-		Random random=new Random();
+
+		Random random = new Random();
 		Scanner in = new Scanner(System.in);
-		Scanner two = new Scanner(System.in);
+
 		int randomNumero;
 		int randomNumero2;
 		int randomNumero3;
 		int pelaajansyottama;
 		String vastaus;
-		
-		
-		System.out.println("Paljonko rahaa laitat sisään?");
+
+		System.out.println("How much money?");
 		vastaus = in.nextLine();
 		pelaajansyottama = Integer.parseInt(vastaus);
-		
-		
-		do
-		{
+
+		do {
 			pelaajansyottama--;
-			System.out.println("Sinulla on rahaa " + pelaajansyottama + "€");
-				randomNumero = random.nextInt(10)+1;
-				randomNumero2 = random.nextInt(10)+1;
-				randomNumero3 = random.nextInt(10)+1;
-			
-					System.out.println("Heitit " + randomNumero);
-						System.out.println("Heitit " + randomNumero2);
-							System.out.println("Heitit " + randomNumero3);
-						
-						if (randomNumero == 7 || randomNumero2 == 7 || randomNumero3 == 7)
-						{
-						System.out.println("VOITIT tällä kierroksella!");
-						pelaajansyottama++ ;
-						}
-						else
-						{ 
-						System.out.println("HÄVISIT tällä kierroksella!");	
-						
-						}
-						
-								if (randomNumero == 7 && randomNumero2 == 7 || randomNumero == 7 && randomNumero3 == 7 || randomNumero2 == 7 && randomNumero3 == 7)
-									{
-									pelaajansyottama+=2;
-									System.out.println("Voitit kivasti hilloo.");
-									}
-							
-								if (randomNumero == 7 && randomNumero2 == 7 && randomNumero3 == 7)
-									{
-									pelaajansyottama+=5;
-									System.out.println("Sie voitit ihan hulluna massia!!!");
-									}
-						
-		
-		System.out.println("Haluatko pelata uuden kierroksen (k/e)?");
-		vastaus = in.nextLine();
-		
-				if (pelaajansyottama <= 0) 
-					{
-					System.out.println("Rahat on loppu. Myy volvosi.");
-					break;
-					}
-				
-					if (vastaus.equals("e")) 
-						{
-						System.out.println("Kiitos pelistä.");
-						break;
-						}
-				
-	} while (vastaus.equals("k"));
-		
-			in.close();	
-				
+			System.out.println("Money " + pelaajansyottama + "€");
+			randomNumero = random.nextInt(10) + 1;
+			randomNumero2 = random.nextInt(10) + 1;
+			randomNumero3 = random.nextInt(10) + 1;
+			System.out.println("You throw " + randomNumero);
+			System.out.println("You throw " + randomNumero2);
+			System.out.println("You throw " + randomNumero3);
+
+			if (randomNumero == 7 || randomNumero2 == 7 || randomNumero3 == 7) {
+				System.out.println("you WIN this round!");
+				pelaajansyottama++;
+			} else {
+				System.out.println("You LOST this round");
+			}
+			if (randomNumero == 7 && randomNumero2 == 7 || randomNumero == 7 && randomNumero3 == 7
+					|| randomNumero2 == 7 && randomNumero3 == 7) {
+				pelaajansyottama += 2;
+				System.out.println("You win!");
+			}
+			if (randomNumero == 7 && randomNumero2 == 7 && randomNumero3 == 7) {
+				pelaajansyottama += 5;
+				System.out.println("Big win!!");
+			}
+			System.out.println("Do you wanna play again (y/n)?");
+			vastaus = in.nextLine();
+			if (pelaajansyottama <= 0) {
+				System.out.println("No money, no funny");
+				break;
+			}
+			if (vastaus.equals("n")) {
+				System.out.println("Thank you for the game.");
+				break;
+			}
+		} while (vastaus.equals("y"));
+		in.close();
 	}
 }
-
-
-
-
-
