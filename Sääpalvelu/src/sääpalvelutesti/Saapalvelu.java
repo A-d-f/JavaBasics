751a1c1c3095ee0016b5be8c427c4d57c5aca1b5
A-d-f <annamaria.nappila@gmail.com>
2022-02-05 19:17:44 +0200

    fixing

--- Sääpalvelu/src/sääpalvelutesti/Saapalvelu.java
+++ Sääpalvelu/src/sääpalvelutesti/Saapalvelu.java
@@ -1,4 +1,5 @@
 package sääpalvelutesti;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,53 +14,47 @@ public class Saapalvelu {
 		int freeze = 4;
 		ArrayList<Double> saapalvelu = new ArrayList<Double>();
 		double annetut;
-		int day=1;
-		
-		
+		int day = 1;
+
 		do
-			
-			{
-				System.out.println("Anna "+ day + " päivän lämpötila: ");
-					annetut = Double.parseDouble(in.nextLine());
-						saapalvelu.add(annetut);
-						day++;
-						
-		 
-			} while (day!=freeze);
-		
-		//int index = saapalvelu.size()-1;
-		//saapalvelu.remove(index);
-		for(int i=0; i<saapalvelu.size();i++)
+
+		{
+			System.out.println("Anna " + day + " päivän lämpötila: ");
+			annetut = Double.parseDouble(in.nextLine());
+			saapalvelu.add(annetut);
+			day++;
+
+		} while (day != freeze);
+
+		// int index = saapalvelu.size()-1;
+		// saapalvelu.remove(index);
+		for (int i = 0; i < saapalvelu.size(); i++)
 			System.out.println(saapalvelu.get(i));
-		
+
 		int index_max = saapalvelu.indexOf(Collections.max(saapalvelu));
 		index_max++;
-		System.out.println("lämpimintä oli "+ index_max + " päivänä " + getMax(saapalvelu) + " astetta");
+		System.out.println("lämpimintä oli " + index_max + " päivänä " + getMax(saapalvelu) + " astetta");
 		DecimalFormat df = new DecimalFormat("0.1");
-		System.out.println("Lämpötilojen keskiarvo: "+ df.format(calculateAverage(saapalvelu)));
-		
-		
-		
+		System.out.println("Lämpötilojen keskiarvo: " + df.format(calculateAverage(saapalvelu)));
+
 		in.close();
-		
+
 	}
-	
-	private static double calculateAverage(ArrayList <Double> saapalvelu) {
-		OptionalDouble average = saapalvelu
-	            .stream()
-	            .mapToDouble(a -> a)
-	            .average();
+
+	private static double calculateAverage(ArrayList<Double> saapalvelu) {
+		OptionalDouble average = saapalvelu.stream().mapToDouble(a -> a).average();
 		return average.getAsDouble();
-}
-	public static double getMax(ArrayList <Double> saapalvelu) {
-		double max= Integer.MIN_VALUE;
-		for(int i=0; i<saapalvelu.size(); i++) {
-			if(saapalvelu.get(i) > max) {
+	}
+
+	public static double getMax(ArrayList<Double> saapalvelu) {
+		double max = Integer.MIN_VALUE;
+		for (int i = 0; i < saapalvelu.size(); i++) {
+			if (saapalvelu.get(i) > max) {
 				max = saapalvelu.get(i);
-				
+
 			}
 		}
 		return max;
 	}
-	
+
 }
